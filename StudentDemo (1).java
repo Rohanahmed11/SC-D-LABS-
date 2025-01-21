3d0d93ad4b141f417f23de7483a56558d69361f0
Rohanahmed11 <roah4500@gmail.com>
2025-01-21 21:37:25 +0000

    Update StudentDemo (1).java

--- StudentDemo (1).java
+++ StudentDemo (1).java
@@ -1,19 +1,16 @@
 package task;
 
-//Immutable Class for Failed Students
 final class FailedStudent {
  private final int rollNumber;
  private final String subjectCode;
  private final String subjectName;
 
- // Constructor
  public FailedStudent(int rollNumber, String subjectCode, String subjectName) {
      this.rollNumber = rollNumber;
      this.subjectCode = subjectCode;
      this.subjectName = subjectName;
  }
 
- // Getter methods
  public int getRollNumber() {
      return rollNumber;
  }
@@ -32,20 +29,17 @@ final class FailedStudent {
  }
 }
 
-//Mutable Class for Promoted Students
 class PromotedStudent {
  private int rollNumber;
  private String subjectCode;
  private String subjectName;
 
- // Constructor
  public PromotedStudent(int rollNumber, String subjectCode, String subjectName) {
      this.rollNumber = rollNumber;
      this.subjectCode = subjectCode;
      this.subjectName = subjectName;
  }
 
- // Getter methods
  public int getRollNumber() {
      return rollNumber;
  }
@@ -58,7 +52,6 @@ class PromotedStudent {
      return subjectName;
  }
 
- // Setter methods
  public void setRollNumber(int rollNumber) {
      this.rollNumber = rollNumber;
  }
@@ -77,20 +70,17 @@ class PromotedStudent {
  }
 }
 
-//Main Class to Test the Implementation
 public class StudentDemo {
  public static void main(String[] args) {
-     // Creating an instance of Immutable FailedStudent
+  
      FailedStudent failedStudent = new FailedStudent(101, "CS101", "Data Structures & Algorithms");
      System.out.println("Immutable Failed Student Details:");
      System.out.println(failedStudent);
 
-     // Creating an instance of Mutable PromotedStudent
      PromotedStudent promotedStudent = new PromotedStudent(102, "CS102", "Operating Systems");
      System.out.println("\nMutable Promoted Student Details:");
      System.out.println(promotedStudent);
 
-     // Modifying details of PromotedStudent
      promotedStudent.setRollNumber(103);
      promotedStudent.setSubjectCode("CS103");
      promotedStudent.setSubjectName("Artificial Intelligence");
